8bf200209da93c72fbd994c212ee43813243a8bc
Sanjay Gupta <8344378+sanjaycg486@users.noreply.github.com>
2020-06-09 14:48:11 +0200

    Added Scatter plot matrix code.

--- Assignments/Assignment 4/Weierstrass-Prize.py
+++ Assignments/Assignment 4/Weierstrass-Prize.py
@@ -1,34 +1,21 @@
-import csv
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
-data, professor, lecture, participant, professional, motivation, presentation, impression = ([], [], [], [], [], [], [], [])
+import plotly.graph_objs as go
+
 # Reading data from file start.
-with open('DataWeierstrass.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile,delimiter=';')
-    for row in reader:
-        data.append(row)
-# print(data)
-for item in range(1, len(data)):
-    for name in range(1):
-        professor.append(data[item][name])
-        lecture.append(int(data[item][name + 1][7:10]))
-        participant.append(int(data[item][name + 2]))
-        professional.append(float(data[item][name + 3]))
-        motivation.append(float(data[item][name + 4]))
-        presentation.append(float(data[item][name + 5]))
-        impression.append(float(data[item][name + 6]))
-# print(professor,lecture,participant,professional,motivation,presentation,impression)
-# print(len(impression))
+dataFrame = pd.read_csv('DataWeierstrass.csv',delimiter=';')
+print(dataFrame)
 # Reading data from file end.
 
 # a) Visualize given data with a scatter plot matrix start.
-# dataFrame = pd.DataFrame(data, columns=['Lecture','Participant','professional','motivation','presentation','impression'])
-# axes = pd.plotting.scatter_matrix(dataFrame, alpha=0.2)
-#
-# # plt.tight_layout()
-# plt.savefig('ScatterplotMatrix.png')
-# plt.show()
-
+fig = go.Figure(data=go.Splom(
+    dimensions=[dict(label='Professor', values=dataFrame['professor']),
+                dict(label='Lecture', values=dataFrame['lecture']),
+                dict(label='Participants', values=dataFrame['participants']),
+                dict(label='Professional expertise', values=dataFrame['professional expertise']),
+                dict(label='Motivation', values=dataFrame['motivation']),
+                dict(label='Clear presentation', values=dataFrame['clear presentation']),
+                dict(label='Overall impression', values=dataFrame['overall impression'])]))
+title = "Scatter plot Matrix."
+fig.update_layout(title=title, width=1200, height=1200)
+fig.show()
 # a) Visualize given data with a scatter plot matrix end.
