6a20d69fb20ce4ba8841f18195f7683576218176
vargo96 <golanivarun21@gmail.com>
2020-06-22 23:36:30 +0200

    Reading the csv data file via Github URL and filtering the data based on the continent 'Europe'

--- Assignments/Assignment 5/COVID-19.py
+++ Assignments/Assignment 5/COVID-19.py
@@ -1,1 +1,7 @@
-# Varun Test commit from Pycharm
+import pandas as pd
+
+# Reading the csv data file via Github URL and filtering the data based on the continent 'Europe'
+data_set_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
+covid19_data_frame = pd.read_csv(data_set_url)
+covid19_data_frame = covid19_data_frame.loc[covid19_data_frame['continent'] == 'Europe']
+print(covid19_data_frame)
