b66975308e094953f256dd140849594211ea4fb3
Sanjay Gupta <8344378+sanjaycg486@users.noreply.github.com>
2020-06-24 12:52:02 +0200

    Added Labels to line graph.

--- Assignments/Assignment 5/COVID-19.py
+++ Assignments/Assignment 5/COVID-19.py
@@ -4,18 +4,20 @@ import plotly.offline as off
 import os
 import plotly.express as px
 
-# Reading the csv data file via Github URL and filtering the data based on the continent 'Europe'
+# Reading the csv data file via Github URL and filtering the data based on the continent 'Europe' start.
 data_set_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
 covid19_data_frame = pd.read_csv(data_set_url)
-covid19_data_frame = covid19_data_frame.loc[covid19_data_frame['continent'] == 'Europe']
+covid19_data_frame = covid19_data_frame.loc[
+    covid19_data_frame['continent'] == 'Europe']  # Filter out data based on Europe continent.
 # print(covid19_data_frame)
-# print(covid19_data_frame.columns) #gives columns of the data frame
+# print(covid19_data_frame.columns)  # Gives columns of the data frame
+# Reading the csv data file via Github URL and filtering the data based on the continent 'Europe' End.
 
 
 if not os.path.exists('assignment5_plots'):
     os.mkdir('assignment5_plots')
 
-# Task 1 from the concept paper
+# Task 1 from the concept paper start.
 # Coded by Varun Nandkumar Golani
 
 countries_in_europe = covid19_data_frame['location'].unique().tolist()
@@ -41,9 +43,11 @@ color_dict = {countries_in_europe[index]: color_list[index]
 print(color_dict)
 
 fig1 = px.line(covid19_data_frame, x='date', y='stringency_index',
+               labels={'date': 'Date', 'stringency_index': 'Government Response Stringency Index 0 to 100 (100 = strictest response).','location': 'Europe countries',
+                       'total_cases': 'Total confirmed cases', 'total_deaths': 'Total deaths', 'new_cases': 'New confirmed cases', 'new_deaths': 'New deaths'},
                color='location', color_discrete_map=color_dict,
                hover_data=['total_cases', 'total_deaths', 'new_cases', 'new_deaths'],
                title='Task 1: Line Graphs for Multivariate Data', height=700)
 
-
 off.plot(fig1, filename='assignment5_plots/task_1_line_graph.html')
+# Task 1 from the concept paper End.
